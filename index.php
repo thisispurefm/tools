8c181c3fe20fce5e4b8f08be04e6f433cf4eeb0e
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2023-10-09 18:56:36 +0100

    make slightly prettier

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Home | PureFM Tools</title>
     <link rel="stylesheet" href="/global/style.css">
     <link rel="shortcut icon" href="/assets/favicon.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/6bd76a095b.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php include($_SERVER['DOCUMENT_ROOT'] . "/global/header.php");?>
@@ -16,10 +17,11 @@
     <section>
     <p style="background-color: #ff6961; height: 3em; text-align: center;"><br>This website is still under construction. Please speak to Thomas with feedback or suggestions for features.</p>
     </section>
-    <section>
-    <p>yes a nicer menu is coming. dont come at me, nice things take time to program.</p>
-    <p><a href="/stats">Broadcast stats</a></p>
-    <p><a href="/avail-s1">Studio 1 availability</a></p>
+
+    <section class="home-menu">
+    <a href="/stats"><i class="fa-regular fa-chart-bar"></i><p>Broadcast stats</p></a>
+    <a href="/avail-s1"><i class="fa-solid fa-calendar"></i><p>Studio 1 availability</p></a>
+    <a href="/avail-s2"><i class="fa-solid fa-calendar"></i><p>Studio 2 availability</p></a>
     </section>
 </main>
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/global/footer.php");?>
