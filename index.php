3a596438b0325558a832586425c2f077c4b67f08
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2023-12-02 15:38:49 +0000

    make material do the material-ing

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
     <title>Document</title>
     <style>
 .demo-card-square.mdl-card {
-  width: 320px;
+  width: 100%;
   height: 320px;
 }
 .demo-card-square > .mdl-card__title {
   color: #fff;
   background:
     url('../assets/demos/dog.png') bottom right 15% no-repeat #46B6AC;
+}
+
+.mdl-grid.center-items {
+  justify-content: center;
 }
 </style>
 </head>
@@ -21,20 +25,71 @@
   <?php include($_SERVER['DOCUMENT_ROOT'] . "/components/nav.php"); ?>
   <main class="mdl-layout__content">
     <div class="page-content">
+    <div class="mdl-grid center-items">
+      <div class="mdl-layout-spacer"></div>
+      <div class="mdl-cell mdl-cell--10-col mdl-typography--text-center">
+        <h1>PureFM Tools</h1>
+      </div>
+      <div class="mdl-layout-spacer"></div>
+    </div>
 
-    <div class="demo-card-square mdl-card mdl-shadow--2dp">
-      <div class="mdl-card__title mdl-card--expand">
-        <h2 class="mdl-card__title-text">Update</h2>
+    <div class="mdl-grid center-items">
+      <!-- NEW LINE -->
+      <div class="mdl-layout-spacer"></div>
+      <div class="mdl-cell mdl-cell--2-col">
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Update</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenan convallis.
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+              View Updates
+            </a>
+          </div>
+        </div>
       </div>
-      <div class="mdl-card__supporting-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aenan convallis.
+      <div class="mdl-layout-spacer"></div>
+      <div class="mdl-cell mdl-cell--2-col">
+      
+       <div class="demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Update</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenan convallis.
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+              View Updates
+            </a>
+          </div>
+        </div>
       </div>
-      <div class="mdl-card__actions mdl-card--border">
-        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-          View Updates
-        </a>
+      <div class="mdl-layout-spacer"></div>
+      <div class="mdl-cell mdl-cell--2-col">
+        <div class="demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">Update</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenan convallis.
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+              View Updates
+            </a>
+          </div>
+          
+        </div>
+        
       </div>
+      <div class="mdl-layout-spacer"></div>
     </div>
   </div>
   </main>
